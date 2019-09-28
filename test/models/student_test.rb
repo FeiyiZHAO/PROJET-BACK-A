0be0e2f918e01fbf1eccc18ccb781d36f8e07f16
Feiyi Zhao <fzhao@utbm.fr>
2019-09-28 12:24:44 +0200

    sqdf

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -5,3 +5,14 @@ class StudentTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+test "should have the necessary required validators" do
+  student = Student.new
+  assert_not student.valid?
+  assert_equal [:name, :last_name], student.errors.keys
+
+  student.name = "Feiyi"
+  student.last_name = "ZHAO"
+  assert student.valid?
+end
+
+end
